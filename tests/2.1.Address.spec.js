411e2f923f0f4391c7f9de8a232645921393c7fb
flaviamedici <flaviaomedici@gmail.com>
2026-01-03 20:12:06 -0800

    Adding more steps to the address test case

--- tests/2.1.Address.spec.js
+++ tests/2.1.Address.spec.js
@@ -1,6 +1,6 @@
+const { test, expect } = require('@playwright/test');
 
-
-test('Add a new address in my account', async ({ page }) => {
+test.only('Add a new address in my account', async ({ page }) => {
 // Navigate to the Testing101 website
 await page.goto('https://www.testing101.net/');
 await page.waitForTimeout(5000);
@@ -17,13 +17,37 @@ await page.getByLabel('Password').fill('Aa123_123');
 //Click on the Login button of the Login form
 await page.getByTestId('buttonElement').click();
 
-// Assertion: Check if 'andriitest7799 account menu' is visible after login
-await expect(page.getByLabel('andriitest7799 account name')).toBeVisible();
-
 //Click on account menu on the header
-await page.getByLabel('andriitest7799 account name').click();
+//await page.getByLabel('andriitest7799 account name').click();
+await page.getByTestId('handle-button').click();
 //Click on My Addresses In the navigation menu
-await page.getByRole('link', { name: 'My Addresses' }).click();
+await page.getByRole('menuitem', { name: 'My Addresses' }).click();
 //Click on the Add New Address button
 await page.frameLocator('iframe[title="My Addresses]').getByRole('button', { name: 'Add New Address' }).click();
+
+//Start the interaction with the iframe
+const frame = await page.frameLocator('//html/body/div[1]/div/div[5]/iframe');
+//Fill the First Name field
+await frame.getByLabel('First Name').fill('Testing');
+//Fill the Last Name field
+await frame.getByLabel('Last Name').fill('101');
+//FIll in Company Name field
+await frame.getByLabel('Company Name').fill('Testing101');
+//Fill in Address field
+await frame.getByLabel('Address', {exact: true }).fill('USA, Seattle');
+//Fill in Address line 2 field
+await frame.getByPlaceholder('Apartment, suite, floor').fill('101');
+//Fill in Address City
+await frame.getByLabel('City').fill('Seattle');
+//Open the Country dropdown
+await frame.getByRole('img').nth(1).click();
+//Choose Option on Drop-down menu
+await frame.getByText('United States').click();
+//Fill in zip code field
+await frame.getByLabel('Zip / Postal code').fill('98101');
+//Fill in phone
+await frame.getByLabel('Phone').fill('+1234567890');
+//Click on Add Address button
+await frame.getByLabel('Add Address and close dialog').click();
+
 });

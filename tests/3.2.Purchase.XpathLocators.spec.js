dd2093c35f65bd31ceb2fee6dc3ab858d20caba3
flaviamedici <flaviaomedici@gmail.com>
2026-01-15 21:03:51 -0800

    Added more steps for the end-to-end flow with xpath locators

--- tests/3.2.Purchase.XpathLocators.spec.js
+++ tests/3.2.Purchase.XpathLocators.spec.js
@@ -16,8 +16,8 @@ await buttonAddToCartPLP.waitFor();
 await buttonAddToCartPLP.click();
 
 //Start interacting with the iframe
-const iframeMinicart = page.frameLocator('xpath=//iframe[contains(@class, "U73P_q")]');
-const buttonViewCart = iframeMinicart.locator('xpath=//footer//span[text()="View Cart"]');
+
+const buttonViewCart = page.locator('xpath=//button//span[text()="View Cart"]');
 await buttonViewCart.waitFor();
 await buttonViewCart.click();
 
@@ -28,8 +28,45 @@ await expect(assertionEmptyCart).not.toBeVisible();
 //Click on the checkout button on the My Cart page
 const buttonCheckout = page.locator('xpath=//span[text()="Checkout"]');
 await buttonCheckout.waitFor();
-await buttonCheckout.click();   
+await buttonCheckout.click();
 
-await page.pause();
+//Checkout step 1
+const fieldEmail = page.locator("xpath=//input[@aria-label='Email']");
+await fieldEmail.fill('test@email.com');
+const fieldFirstName = page.locator("xpath=//input[@aria-label='First name']")
+await fieldFirstName.fill('Mary');
+const fieldLastName = page.locator("xpath=//input[@aria-label='Last name']")
+await fieldLastName.fill('Smith');
+const fieldPhone = page.locator("xpath=//input[@aria-label='Phone']")
+await fieldPhone.fill('123-456-7890');
+
+//Locate country dropdown input
+const dropdownCountryRegion = page.locator("xpath=//div[@data-hook='form-field-country']");
+await dropdownCountryRegion.click();
+const dropdownOption = page.locator("xpath=//div[text()='Canada']");
+await dropdownOption.click();
+
+const fieldAddress = page.locator("xpath=//div[@data-hook='form-field-address_line']//input[@role='combobox']");
+await fieldAddress.fill('123 Maple Street')
+
+const fieldCity = page.locator("xpath=//input[@aria-label='City']");
+await fieldCity.fill('Toronto');
+
+const dropdownProvince = page.locator("xpath=//*[@id='form-field-input-104cd079-756e-4fa1-6f33-07f1a9969567-TPAMultiSection_ltbvfekh-eb42bc72']")
+await dropdownProvince.click();
+const dropdownProvinceOption = page.locator("xpath=//div[text()='Ontario']");
+await dropdownProvinceOption.click();
+
+const fieldZipCode = page.locator("xpath=//input[@aria-label='Zip / Postal Code']");
+await fieldZipCode.fill('M4B 1B3');
+
+const buttonContinueCheckout = page.locator("xpath=//span[text()='Continue']");
+await buttonContinueCheckout.click();
+
+const buttonContinueCheckout2 = page.locator("xpath=//span[text()='Continue']");
+await buttonContinueCheckout2.click();
+
+const buttonPlaceOrder = page.locator("xpath=//span[text()='Place Order & Pay']");
+await buttonPlaceOrder.click();
 
 });
